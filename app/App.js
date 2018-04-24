cfe6f76331d8ee7440d2056b7ceae56641edd89b
Olga Pigareva <olga@pigareva.de>
2018-04-24 16:07:02 +0200

    create Greeting component

--- app/App.js
+++ app/App.js
@@ -426,6 +426,26 @@ class Clock extends Component {
   }
 }
 
+class Greeting extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const hh = this.state.time/60;
+    switch (true) {
+      case (hh >= 8 && hh <=10):
+        return <h2>  'Good Morning!' </h2>;
+      case (hh >= 12 && hh <=14):
+        return <h2>  'Bon appetite!'</h2>;
+      case (hh >= 17 && hh <=20):
+        return <h2>  'Bye bye!'</h2>;
+      default:
+        return null;
+    }
+  }
+}
+
 class EmployeeOnDesk extends Component {
   constructor(props) {
     super(props);
