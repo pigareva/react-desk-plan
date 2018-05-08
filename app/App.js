c8bf198d8eb15bb0fea9c54803fe168bc3965c6e
Olga Pigareva <olga@pigareva.de>
2018-05-08 14:49:06 +0200

    get employees from data bank

--- app/App.js
+++ app/App.js
@@ -4,220 +4,7 @@ import { Card, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 import '../app/style/style.scss';
 
-const employees = [
-  {
-    _id: '01',
-    department: 'Founder',
-    name: 'Harry Potter',
-    email: 'potter@pigareva.de',
-    photo: '',
-  },
-  {
-    _id: '02',
-    department: 'Founder',
-    name: 'Albus Dumbledore',
-    email: 'dumbledore@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '03',
-    department: 'HR',
-    name: 'Hermione Granger',
-    email: 'granger@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '30',
-    department: 'HR',
-    name: 'Minerva McGonagall',
-    email: 'granger@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '04',
-    department: 'Finance',
-    name: 'Mad-Eye Moody',
-    email: 'moody@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '05',
-    department: 'Tech',
-    name: 'Ron Weasley',
-    email: 'weasley@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '06',
-    department: 'Tech',
-    name: 'Rubeus Hagrid',
-    email: 'hagrid@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '07',
-    department: 'Tech',
-    name: 'Sirius Black',
-    email: 'black@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '08',
-    department: 'Tech',
-    name: 'Neville Longbottom',
-    email: 'longbottom@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '09',
-    department: 'Tech',
-    name: 'Ron Weasley',
-    email: 'weasley@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '10',
-    department: 'Tech',
-    name: 'Cedric Diggory',
-    email: 'diggory@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '11',
-    department: 'B2B Sales',
-    name: 'Bellatrix Lestrange',
-    email: 'lestrange@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '12',
-    department: 'B2B Sales',
-    name: 'Draco Malfoy',
-    email: 'malfoy@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '13',
-    department: 'B2B Sales',
-    name: 'Gellert Grindelwald',
-    email: 'grindelwald@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '14',
-    department: 'B2B Sales',
-    name: 'Bartemius (Barty) Crouch, Jr.',
-    email: 'crouch@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '15',
-    department: 'B2B Sales',
-    name: 'Peter Pettigrew',
-    email: 'pettigrew@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '16',
-    department: 'B2B Sales',
-    name: 'Tom Riddle',
-    email: 'voldemort@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '17',
-    department: 'Customer care',
-    name: 'Salazar Slytherin ',
-    email: 'slytherin@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '18',
-    department: 'Customer care',
-    name: 'Vincent Crabbe',
-    email: 'crabbe@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '19',
-    department: 'Customer care',
-    name: 'Millicent Bulstrode',
-    email: 'bulstrode@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '20',
-    department: 'Customer care',
-    name: 'Marcus Flint',
-    email: 'flint@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '21',
-    department: 'Customer care',
-    name: 'Gregory Goyle',
-    email: 'goyle@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '22',
-    department: 'Customer care',
-    name: 'Graham Montague',
-    email: 'montague@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '23',
-    department: 'Product Team',
-    name: 'Igor Karkaroff',
-    email: 'karkaroff@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '24',
-    department: 'Product Team',
-    name: 'Viktor Krum',
-    email: 'krum@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '25',
-    department: 'Marketing',
-    name: 'Dolores Jane Umbridge',
-    email: 'umbridge@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '26',
-    department: 'Marketing',
-    name: 'Argus Filch',
-    email: 'filch@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '27',
-    department: 'Product Team',
-    name: 'Severus Snape',
-    email: 'snape@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '28',
-    department: 'Working students',
-    name: 'John Doe',
-    email: 'john@pigareva.cc',
-    photo: '',
-  },
-  {
-    _id: '31',
-    department: 'Working students',
-    name: 'Dudley Dursley',
-    email: 'dursley@pigareva.de',
-    photo: '',
-  },
-];
-
-function getDepartmentsList() {
+function getDepartmentsList(employees) {
   const departments = [];
   employees.forEach((employee) => {
     if (!departments.includes(employee.department)) {
@@ -227,7 +14,7 @@ function getDepartmentsList() {
   return departments;
 }
 
-function getDepartmentsWithEmployees() {
+function getDepartmentsWithEmployees(employees) {
   const departmentIndex = {};
 
   employees.forEach((employee) => {
@@ -246,9 +33,9 @@ function getDepartmentsWithEmployees() {
   return departmentIndex;
 }
 
-function getTables() {
+function getTables(departmentIndex) {
   const tables = [];
-  const departmentsWithEmployees = getDepartmentsWithEmployees();
+  const departmentsWithEmployees = departmentIndex;
   for (let department in departmentsWithEmployees) {
     tables.push(departmentsWithEmployees[department]);
   }
@@ -382,8 +169,37 @@ class TableWithEmployees extends Component {
 }
 
 class OfficeRoom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { employees: {
+        error: null,
+        isLoaded: false,
+        employees: []
+      }};
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8000/employees")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({ employees: {
+              isLoaded: true,
+              employees: result
+            }});
+        },
+        (error) => {
+          this.setState({ employees: {
+              isLoaded: true,
+              error
+            }});
+        }
+      )
+  }
+
   render() {
-    const tables = getTables()
+    const { error, isLoaded, employees } = this.state.employees;
+    const tables = getTables(getDepartmentsWithEmployees(employees))
       .map(table => <TableWithEmployees table={table} key={table.department} />);
 
     return (
