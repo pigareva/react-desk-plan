6bd595060db1e1e7cf4af6274bf699fcbb2a1d82
Olga Pigareva <olga@pigareva.de>
2018-05-08 12:19:36 +0200

    add eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+      "browser": true
+    },
+    "rules": {
+        "no-unused-vars": 1,
+        "prefer-destructuring": 1,
+        "react/prefer-stateless-function": 1,
+      }
+};
