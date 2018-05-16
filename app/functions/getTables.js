d15af0db3243fd9adf99ccb2bc50dc0f35524d4c
Olga Pigareva <olga@pigareva.de>
2018-05-16 14:21:53 +0200

    optimize getTables()

--- app/functions/getTables.js
+++ app/functions/getTables.js
@@ -8,9 +8,6 @@ export default function getTables(departmentIndex) {
   if (!departmentIndex) {
     throw new TypeError('`departmentIndex` is not defined');
   }
-  const tables = [];
-  for (const department in departmentIndex) {
-    tables.push(departmentIndex[department]);
-  }
-  return tables;
+
+  return Object.values(departmentIndex);
 }
