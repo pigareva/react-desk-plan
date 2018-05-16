4ad5d95e978a773d4d6de06b476266941df74e5b
Olga Pigareva <olga@pigareva.de>
2018-05-16 14:10:00 +0200

    add port consts

--- app/consts.js
+++ app/consts.js
@@ -1,3 +1,5 @@
-const URL_EMPLOYEES = 'http://localhost:8000/employees';
+export const FRONTEND_PORT = '8080';
+export const BACKEND_PORT = '8000';
 
+const URL_EMPLOYEES = `http://localhost:${BACKEND_PORT}/employees`;
 export const URL_GET_ALL_EMPLOYEES = URL_EMPLOYEES;
