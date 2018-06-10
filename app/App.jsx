c55239e5a62c60f080200e637f7fc40b395a60f0
Olga Pigareva <olga@pigareva.de>
2018-06-10 19:19:27 +0200

    install bootstrap and jquery

--- app/App.jsx
+++ app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import OfficeRoom from './components/OfficeRoom';
 import ErrorBoundary from './components/ErrorBoundary';
 
