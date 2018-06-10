c0a424ac3a76c77ee8d0f769f589b1f3e68959c9
Olga Pigareva <olga@pigareva.de>
2018-06-10 19:13:46 +0200

    add update and create consts

--- app/consts.js
+++ app/consts.js
@@ -5,3 +5,5 @@ const URL_EMPLOYEES = `http://localhost:${BACKEND_PORT}/employees`;
 
 export const URL_GET_ALL_EMPLOYEES = URL_EMPLOYEES;
 export const URL_DELETE_EMPLOYEE = `${URL_EMPLOYEES}/delete/:`;
+export const URL_UPDATE_EMPLOYEE = `${URL_EMPLOYEES}/update/:`;
+export const URL_CREATE_EMPLOYEE = `${URL_EMPLOYEES}/create`;
