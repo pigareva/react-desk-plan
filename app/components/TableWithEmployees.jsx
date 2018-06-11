b72a919b1956bd553c4fdc2de761f1ea7aeae1fe
Olga Pigareva <olga@pigareva.de>
2018-06-11 09:34:08 +0200

    make table with employees a stateless function

--- app/components/TableWithEmployees.jsx
+++ app/components/TableWithEmployees.jsx
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import EmployeeOnDesk from './EmployeeOnDesk';
 
-export default class TableWithEmployees extends Component {
-  render() {
-    const employeesOnTable = this.props.table.employees
-      .map(employee => <EmployeeOnDesk employee={employee} key={employee._id} />);
-
-    return (
-      <div className="table-flex-block" key={this.props.table.department}>
-        <h1 className="text-center">{this.props.table.department}</h1>
-        <div className="desk-flex-container">
-          {employeesOnTable}
-        </div>
+const TableWihEmployees = (props) => {
+  const employeesOnTable = props.table.employees
+    .map(employee => <EmployeeOnDesk employee={employee} key={employee._id} />);
+  return (
+    <div className="table-flex-block" key={props.table.department}>
+      <h1 className="text-center">{props.table.department}</h1>
+      <div className="desk-flex-container">
+        {employeesOnTable}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-TableWithEmployees.propTypes = {
-  table: PropTypes.object.isRequired,
+export default TableWihEmployees;
+
+TableWihEmployees.propTypes = {
+  table: PropTypes.objectOf(PropTypes.any).isRequired,
 };
