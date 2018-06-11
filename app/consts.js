610ac872d71c93ba02b0dd4c8e5671eef00d9fea
Olga Pigareva <olga@pigareva.de>
2018-06-11 12:40:07 +0200

    delete unnecessary double dots for consts

--- app/consts.js
+++ app/consts.js
@@ -4,6 +4,6 @@ export const BACKEND_PORT = '8000';
 const URL_EMPLOYEES = `http://localhost:${BACKEND_PORT}/employees`;
 
 export const URL_GET_ALL_EMPLOYEES = URL_EMPLOYEES;
-export const URL_DELETE_EMPLOYEE = `${URL_EMPLOYEES}/delete/:`;
-export const URL_UPDATE_EMPLOYEE = `${URL_EMPLOYEES}/update/:`;
+export const URL_DELETE_EMPLOYEE = `${URL_EMPLOYEES}/delete/`;
+export const URL_UPDATE_EMPLOYEE = `${URL_EMPLOYEES}/update/`;
 export const URL_CREATE_EMPLOYEE = `${URL_EMPLOYEES}/create`;
