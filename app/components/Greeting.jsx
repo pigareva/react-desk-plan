1b2ebc8d751d7b3548d4f926ea174802fbf5f702
Olga Pigareva <olga@pigareva.de>
2018-06-13 11:43:04 +0200

    Greeting: make it stateless func

--- app/components/Greeting.jsx
+++ app/components/Greeting.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Greeting extends Component {
-  render() {
-    const hh = this.props.time / 60;
-    switch (true) {
-      case (hh >= 8 && hh <= 10):
-        return <div className="greeting"> Good morning! </div>;
-      case (hh >= 12 && hh <= 14):
-        return <div className="greeting"> Bon appetite! </div>;
-      case (hh >= 17 && hh <= 20):
-        return <div className="greeting"> Bye bye! </div>;
-      default:
-        return null;
-    }
+const Greeting = (props) => {
+  const hh = props.time / 60;
+  switch (true) {
+    case (hh >= 8 && hh <= 10):
+      return <div className="greeting"> Good morning! </div>;
+    case (hh >= 12 && hh <= 14):
+      return <div className="greeting"> Bon appetite! </div>;
+    case (hh >= 17 && hh <= 20):
+      return <div className="greeting"> Bye bye! </div>;
+    default:
+      return null;
   }
-}
+};
+
+export default Greeting;
 
 Greeting.propTypes = {
   time: PropTypes.number.isRequired,
