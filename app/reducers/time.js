dc3ef1122b5707933435f977122bf636a66a86cf
Olga Pigareva <olga@pigareva.de>
2018-06-15 08:11:09 +0200

    delete console logs from reduser

--- app/reducers/time.js
+++ app/reducers/time.js
@@ -1,11 +1,9 @@
 import { CURRENT_TIME, NEW_DAY, NEW_HOUR, START_WORKING_DAY_TIME } from '../consts';
 
 const time = (state = {}, action) => {
-  console.log('reduser', state, action);
   switch (action.type) {
     case CURRENT_TIME:
     {
-      console.log('reduser current time: ', action.time);
       return Object.assign({}, state, { time: action.time });
     }
     case NEW_HOUR:
