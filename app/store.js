18f624527592c145351d2727b85d7360e0b0e506
Olga Pigareva <olga@pigareva.de>
2018-06-15 12:08:45 +0200

    add browser extention as store argument

--- app/store.js
+++ app/store.js
@@ -1,5 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const vanillaPromise = store => next => (action) => {
+  if (typeof action.then !== 'function') {
+    return next(action);
+  }
+
+  return Promise.resolve(action).then(store.dispatch);
+};
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(vanillaPromise),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+));
+
 export default store;
