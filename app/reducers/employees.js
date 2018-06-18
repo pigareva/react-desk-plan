8c2a1a01f20231f89b22173ec15ce1244b2490d6
Olga Pigareva <olga@pigareva.de>
2018-06-18 15:29:05 +0200

    add create and update actions to employee reduser

--- app/reducers/employees.js
+++ app/reducers/employees.js
@@ -1,10 +1,31 @@
-import { EMPLOYEES_LOADED } from '../consts';
+import { EMPLOYEE_CREATED, EMPLOYEE_UPDATED, EMPLOYEES_LOADED } from '../consts';
 
 const employees = (state = {}, action) => {
   switch (action.type) {
     case EMPLOYEES_LOADED:
     {
       return Object.assign({}, state, { employeesData: action.employeesData });
+    }
+    case EMPLOYEE_CREATED:
+    {
+      const { employeesData } = state;
+      employeesData.push(action.employee);
+      return Object.assign({}, state, {
+        employeesData,
+      });
+    }
+    case EMPLOYEE_UPDATED:
+    {
+      const data = state.employeesData;
+      data.forEach((employee) => {
+        if (employee._id === action.employee._id) {
+          // eslint-disable-next-line no-param-reassign
+          Object.keys(employee).forEach((key) => { employee[key] = action.employee[key]; });
+        }
+      });
+      return Object.assign({}, state, {
+        employeesData: data,
+      });
     }
     default:
       return state;
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import time from './time';
 import employees from './employees';
+import button from './button';
 
 export default combineReducers({
   time,
   employees,
+  button,
 });
