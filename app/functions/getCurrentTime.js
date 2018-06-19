2398f5784386b5c5c91eedbaa6a45441add7e409
Olga Pigareva <olga@pigareva.de>
2018-06-19 13:32:25 +0200

    fix getCurrentTime

--- app/functions/getCurrentTime.js
+++ app/functions/getCurrentTime.js
@@ -1,17 +1,31 @@
-import { currentTime } from '../actions';
+import { currentTime, endDay } from '../actions';
 import store from '../store';
 import { END_WORKING_DAY_TIME, START_WORKING_DAY_TIME, TIME_INTERVAL } from '../consts';
 
 const getCurrentTime = (time) => {
   let res = time.time || START_WORKING_DAY_TIME;
+
+  const tick = () => {
+    if (!store.getState().button.buttonStart) {
+      res = res >= END_WORKING_DAY_TIME ? END_WORKING_DAY_TIME : res + 1;
+
+      store.dispatch(currentTime(res));
+
+      if (res === END_WORKING_DAY_TIME) {
+        store.dispatch(endDay);
+        // eslint-disable-next-line no-use-before-define
+        stopTimer();
+      }
+    }
+  };
+
   const currentTimerID = setInterval(() => {
-    res = res >= END_WORKING_DAY_TIME ? END_WORKING_DAY_TIME : res + 1;
-    store.dispatch(currentTime(res));
+    tick();
   }, TIME_INTERVAL);
 
-  if (res === END_WORKING_DAY_TIME) {
+  const stopTimer = () => {
     clearInterval(currentTimerID);
-  }
+  };
 };
 
 export default getCurrentTime;
