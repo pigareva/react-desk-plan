a407eafb654d8220500b0bed22098f70eee07376
Olga Pigareva <olga@pigareva.de>
2018-06-26 08:25:10 +0200

    set up tests

---
+++ setupTests.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { configure, shallow, render, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
