e5f049c299653478abfb84d42dd2a27be4cd9280
Olga Pigareva <olga@pigareva.de>
2018-07-04 13:33:19 +0200

    Department: rename variables

--- app/components/Department.jsx
+++ app/components/Department.jsx
@@ -7,14 +7,14 @@ import getTime from '../functions/getTime';
 const Department = (props) => {
   const delays = props.department.employees.map(employee => employee.delay);
   const delay = Math.floor(delays.reduce((a, b) => a + b) / delays.length);
-  const employeesAtDepartment = props.department.employees
+  const employeesInDepartment = props.department.employees
     .map(employee => <EmployeeOnDeskWithClockRestart employee={employee} key={employee._id} />);
   return (
     <div className="table-flex-block" key={props.department.department}>
       <h1 className="text-center">{props.department.department}</h1>
       <h4 className="text-center">Expected coming time is {getTime(START_WORKING_DAY_TIME + delay)}</h4>
       <div className="desk-flex-container">
-        {employeesAtDepartment}
+        {employeesInDepartment}
       </div>
     </div>
   );
