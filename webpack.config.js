9ad74cc7cc38623aa426d98f56ab7efacdbf42ca
Olga Pigareva <olga.pigareva@deepl.com>
2021-01-11 19:39:20 +0100

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 const path = require('path');
 const BabelWebpackPlugin = require('babel-minify-webpack-plugin');
-
-const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : 'development';
+// const { ModuleFederationPlugin } = require('webpack').container;
 const webpack = require('webpack');
 
+const NODE_ENV_DEVELOPMENT = 'development';
+const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : NODE_ENV_DEVELOPMENT;
+
 const plugins = [
+  // new ModuleFederationPlugin({
+  //   name: 'app1',
+  //   remotes: {
+  //     app2: 'app2_remote',
+  //   },
+  //   exposes: { './potterApp': './app/App' },
+  //   shared: ['react', 'react-dom'],
+  // }),
   // Create a NODE_ENV const https://webpack.js.org/plugins/define-plugin
   new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(NODE_ENV) }),
 ];
 
-if (NODE_ENV === 'development') {
+if (NODE_ENV === NODE_ENV_DEVELOPMENT) {
   plugins.push(new webpack.HotModuleReplacementPlugin());
 }
 
@@ -19,19 +29,26 @@ if (NODE_ENV === 'production') {
 
 module.exports = {
   entry: path.resolve(__dirname, 'app/App.jsx'),
+  // todo
+  // entry: {
+  //   home: path.resolve(__dirname, 'app/App.jsx'),
+  //   room: {
+  //     import: './app/containers/OfficeRoomWithEmployees',
+  //     filename: 'app/containers/OfficeRoomWithEmployees.js',
+  //   },
+  // },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public/'),
-    publicPath: '/',
   },
-  watch: NODE_ENV !== 'production',
+  mode: NODE_ENV,
   watchOptions: {
     poll: 1000,
     aggregateTimeout: 100,
   },
 
   // Add source map only for dev env
-  devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : false,
+  devtool: NODE_ENV === 'development' ? 'source-map' : 'eval',
 
   // Define where to look for a plugin
   resolve: {
@@ -42,7 +59,6 @@ module.exports = {
   // Define where to look for a webpack loader
   resolveLoader: {
     modules: ['node_modules'],
-    moduleExtensions: ['-loader'],
     extensions: ['.js'],
   },
 
@@ -69,7 +85,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style', 'css'],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(js|jsx)$/,
@@ -79,17 +95,26 @@ module.exports = {
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         include: path.resolve(__dirname, 'public/img'),
-        use: [{ loader: 'file-loader' }],
+        type: 'asset/resource',
       },
     ],
   },
 
   devServer: {
-    contentBase: './public',
+    contentBase: path.join(__dirname, 'public'),
     hot: true,
     historyApiFallback: true,
     inline: true,
   },
 
   plugins,
+
+  // todo
+  // optimization: {
+  //   splitChunks: {
+  //     minSize: {
+  //       javascript: 30000,
+  //     },
+  //   },
+  // },
 };
